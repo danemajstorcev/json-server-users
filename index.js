f049ced9c41e0cfc9ded72d371564d5ae3480582
Dane.Majstorcev-FE12 <majstorcev.dane@gmail.com>
2024-03-04 21:20:50 +0100

    one user

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
-server.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type, Access-Control-Allow-Origin, Authorization",
-  })
-);
-
+server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
