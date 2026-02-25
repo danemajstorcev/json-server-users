5280e2a68a4c28d3adf14fd4370a27ea872b77bb
Dane.Majstorcev-FE12 <majstorcev.dane@gmail.com>
2026-02-25 12:12:58 +0100

    changed

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const jsonServer = require("json-server");
-const cors = require("cors");
-const path = require("path");
-
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-server.use(cors());
-server.use(jsonServer.bodyParser);
 server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  })
+);
 server.use(router);
 
-const PORT = 8001;
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
-});
+module.exports = server;
